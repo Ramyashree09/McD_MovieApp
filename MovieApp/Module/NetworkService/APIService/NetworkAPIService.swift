e6a9943dc98042b082cfc158a6464044dd9105e5
Ramyashree S <ramyashree.a.s@capgemini.com>
2025-06-11 19:35:25 +0530

    Refactored NetworkService module

--- MovieApp/Module/NetworkService/APIService/NetworkAPIService.swift
+++ MovieApp/Module/NetworkService/APIService/NetworkAPIService.swift
@@ -10,12 +10,9 @@ import SwiftUI
 
 class APIService {
     static let shared = APIService()
-
     private init() {}
-    
     //Fetch data from API by creating valid url
     func fetchRequest<T: Decodable>(urlString: String, model : T.Type) async throws -> T {
-        
         guard let url = URL(string: urlString) else {
             print("Invalid URL: \(urlString)") //debugging
             throw URLError(.badURL)
@@ -23,13 +20,11 @@ class APIService {
         guard Reachability.isConnectedToNetwork() else {
             throw URLError(.notConnectedToInternet)
         }
-        
         let (data, response) = try await URLSession.shared.data(from: url)
-        
+
         guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
             throw URLError(.badServerResponse)
         }
-        
         return try JSONDecoder().decode(model, from: data)
     }
 }
